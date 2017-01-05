e53e20557fa9bf0bf485554a6a561f5601a0e6d6
TrungQuanAt <trungquantdhtdt@gmail.com>
2017-01-05 10:43:05 +0700

    Homework

---
+++ trungquan/sum-subtract/scr/subtract.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <subtract.h>
+int sub_function(int a, int b);
+
+int sub_function(int a, int b)
+{
+ printf("hieu 2 so %d va %d la\n",a,b);
+ return(a-b);
+}
---
+++ trungquan/sum-subtract/scr/sum.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <sum.h>
+int sum_function(inta, intb);
+
+int sum_function(int a, int b)
+{
+ printf("tong 2 so %d va %d la\n",a,b);
+ return(a+b);
+}
