2b40cfc656764c379c0adc3fb457de09c7d84d0c
duyphan <phanduy.2111995@outlook.com.vn>
2017-01-06 17:07:54 +0700

    Them bt sum-subtract

---
+++ duyphan/baitap/sum_subtract/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "sum.h"
+#include "subtract.h"
+
+int main(int argc, char *argv[])
+{
+	unsigned long kq;
+	kq = sum_function(30, 10);
+	printf("kq: %ld\n", kq);
+	kq = sub_function(30, 10);
+	printf("kq: %ld\n", kq);
+
+	return 0;
+}
---
+++ duyphan/baitap/sum_subtract/src/sum.c
@@ -0,0 +1,9 @@
+/*filename: sum.c*/
+#include <stdio.h>
+#include <sum.h>
+
+int sum_function(int a, int b)
+{
+	printf("Tong 2 so %d va %d la\n", a, b);
+	return (a+b);
+}
