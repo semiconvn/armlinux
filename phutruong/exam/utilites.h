fa1844028d09b6c2bfa662b0f74d39f13f2b6c2c
ngocphu811 <ngocphu811@live.com>
2017-03-27 20:42:05 +0700

    adding exam skelton code

--- phutruong/exam/utilites.h
+++ phutruong/exam/utilites.h
@@ -1,1 +1,14 @@
 
+
+typedef struct 
+{
+	uint8_t msg_type;
+	void*   data_ptr;
+} itc_msg_t;
+
+/*Interthread communication apis*/
+int  itc_send_msg(itc_msg_t*  send_msg, uint32_t timeout);
+int  itc_recv_msg(itc_msg_t*  recv_msg, uint32_t timeout);
+
+int  util_keyToFreq(uint8_t inKeyVal);
+ 
